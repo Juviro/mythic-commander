adadb98bfe88b5d8b2e4423a7dad5b0b568925e7
Hauke Witte <hw@skalar.no>
2020-01-18 23:35:37 +0100

    move actions to one file

--- frontend/src/components/Decks/Deck/DeckOverview/Actions/Actions.jsx
+++ frontend/src/components/Decks/Deck/DeckOverview/Actions/Actions.jsx
@@ -1,32 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { Menu, Dropdown, Icon } from 'antd';
+import { Dropdown, Icon } from 'antd';
 
-import ActionDelete from './ActionDelete';
-import ActionChangeSet from './ActionChangeSet';
-import ActionChangeZone from './ActionChangeZone';
-import ActionAddOrRemoveFromCollection from './ActionAddOrRemoveFromCollection';
+import ActionsMenu from './ActionsMenu';
 
 const StyledActions = styled.div`
   padding-left: 20px;
 `;
 
 export default ({ card }) => {
-  const [isVisible, setIsVisible] = useState(false);
-
-  const menu = (
-    <Menu>
-      <ActionAddOrRemoveFromCollection card={card} setIsVisible={setIsVisible} />
-      <ActionChangeSet card={card} setIsVisible={setIsVisible} />
-      <ActionChangeZone card={card} setIsVisible={setIsVisible} />
-      <Menu.Divider />
-      <ActionDelete card={card} setIsVisible={setIsVisible} />
-    </Menu>
-  );
-
   return (
     <StyledActions>
-      <Dropdown overlay={menu} placement="bottomLeft" trigger={['click']}>
+      <Dropdown overlay={<ActionsMenu card={card} />} placement="bottomLeft" trigger={['click']}>
         <Icon type="ellipsis" />
       </Dropdown>
     </StyledActions>
