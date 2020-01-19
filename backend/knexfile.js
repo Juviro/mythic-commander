bbc7350ba476d3bc3f8c6e797a15cf3e46406b5e
Hauke Witte <hw@skalar.no>
2020-01-19 21:55:09 +0100

    small fix’

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,7 +1,10 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/mtg',
+    connection: {
+      database: 'mtg',
+      host: 'localhost',
+    },
     useNullAsDefault: true,
     migrations: {
       directory: './src/database/migrations',
@@ -14,7 +17,7 @@ module.exports = {
       user: 'hauke',
       database: 'mtg',
       password: 'dqwfhife83hfwkrh38fnwfbcxsi3r2uu9fcewrb321f90d8fy2b3r98dsfye9vhdqww83rnfY',
-      host: 'postgres://localhost/mtg',
+      host: 'localhost',
     },
     migrations: {
       directory: './src/database/migrations',
