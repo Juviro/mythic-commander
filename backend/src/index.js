71a0cc2d301c79f0a77e1705da0ded039d8dddf8
Hauke Witte <hw@skalar.no>
2020-01-19 21:25:38 +0100

    added backend api path

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,5 +5,5 @@ import './cron';
 const app = new Koa();
 const port = process.env.PORT || '4000';
 
-server.applyMiddleware({ app });
+server.applyMiddleware({ app, path: '/mtg-api/graphql' });
 app.listen({ port }, () => console.info(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`));
