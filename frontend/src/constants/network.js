1e572e4d3cdbb9bf38e53dce341d65c4459cb21a
Hauke Witte <hw@skalar.no>
2020-01-19 21:37:01 +0100

    small fix’

--- frontend/src/constants/network.js
+++ frontend/src/constants/network.js
@@ -1,3 +1,3 @@
 const IS_DEV = process.env.NODE_ENV === 'development';
 
-export const SERVER_URL = IS_DEV ? 'http://localhost:4000/graphql' : 'https://hauke.uber.space/mtg-api/graphql';
+export const SERVER_URL = IS_DEV ? 'http://localhost:4000/mtg-api/graphql' : 'https://hauke.uber.space/mtg-api/graphql';
