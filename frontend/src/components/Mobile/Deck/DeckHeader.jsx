8c852dfa6dadaed23c12891cddf4e553d12e4100
Hauke Witte <hw@skalar.no>
2020-01-22 21:44:12 +0100

    replaced favicon

--- frontend/src/components/Mobile/Deck/DeckHeader.jsx
+++ frontend/src/components/Mobile/Deck/DeckHeader.jsx
@@ -3,27 +3,24 @@ import { Affix, Menu } from 'antd';
 import styled from 'styled-components';
 import { withRouter } from 'react-router';
 
-const StyledHeader = styled.div`
-  width: 100%;
-`;
-
 const StyledMenuItem = styled(Menu.Item)`
   width: 33.33%;
   display: flex !important;
   justify-content: center;
 `;
+const StyledAffix = styled(Affix)`
+  width: 100%;
+`;
 
 const DeckList = ({ deck }) => {
   return (
-    <StyledHeader>
-      <Affix offsetTop={50}>
-        <Menu defaultSelectedKeys="cards" mode="horizontal" style={{ display: 'flex' }}>
-          <StyledMenuItem key="cards">Cards</StyledMenuItem>
-          <StyledMenuItem key="stats">Stats</StyledMenuItem>
-          <StyledMenuItem key="edit">Edit</StyledMenuItem>
-        </Menu>
-      </Affix>
-    </StyledHeader>
+    <Affix offsetTop={50} style={{ width: '100%' }}>
+      <Menu defaultSelectedKeys="cards" mode="horizontal" style={{ display: 'flex' }}>
+        <StyledMenuItem key="cards">Cards</StyledMenuItem>
+        <StyledMenuItem key="stats">Stats</StyledMenuItem>
+        <StyledMenuItem key="edit">Edit</StyledMenuItem>
+      </Menu>
+    </Affix>
   );
 };
 
--- frontend/src/components/Mobile/Deck/DeckImage.jsx
+++ frontend/src/components/Mobile/Deck/DeckImage.jsx
@@ -4,27 +4,19 @@ import styled from 'styled-components';
 import { withRouter } from 'react-router';
 import { Parallax } from 'react-parallax';
 
-const StyledHeader = styled.div`
+const StyledAffix = styled(Affix)`
   width: 100%;
   display: flex;
   flex-direction: column;
 `;
 
-const StyledHeaderImage = styled.img`
-  width: 100%;
-  height: 100%;
-`;
-
 const DeckList = ({ deck: { imgSrc } }) => {
   return (
-    <StyledHeader>
-      <Affix offsetTop={-150}>
-        <Parallax strength={300} bgImage={imgSrc} bgImageStyle={{ width: '100%', height: 'auto', top: 50 }}>
-          <div style={{ height: 200 }} />
-          {/* <StyledHeaderImage src={imgSrc} /> */}
-        </Parallax>
-      </Affix>
-    </StyledHeader>
+    <StyledAffix offsetTop={-150}>
+      <Parallax strength={300} bgImage={imgSrc} bgImageStyle={{ width: '100%', height: 'auto', top: 50 }}>
+        <div style={{ height: 200 }} />
+      </Parallax>
+    </StyledAffix>
   );
 };
 
