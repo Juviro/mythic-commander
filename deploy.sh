f96f747dbdeffaf4031a8995106e9da3eb1fce20
Hauke Witte <hw@skalar.no>
2020-01-22 16:02:50 +0100

    small fix’

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo -e "\e[93mBuilding frontend"
+echo -e "\e[93mBuilding frontend\e[39"
 (cd /home/hauke/packages/mtg/frontend && yarn install --production=false && yarn build)
 
-echo -e "\e[93mBuilding backend"
+echo -e "\e[93mBuilding backend\e[39"
 (cd /home/hauke/packages/mtg/backend && yarn install --production=false && yarn build)
 
-echo -e "\e[93mdeploying frontend"
+echo -e "\e[93mdeploying frontend\e[39"
 rm -rf /var/www/virtual/hauke/html/* 
 cp -r /home/hauke/packages/mtg/frontend/build/* /var/www/virtual/hauke/html/
 
-echo -e "\e[93mrestarting backend"
+echo -e "\e[93mrestarting backend\e[39"
 supervisorctl restart mtg
