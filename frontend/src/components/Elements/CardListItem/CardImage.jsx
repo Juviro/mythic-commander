823b4f9bbd15703d873b3bc62c8fded2d1cbe8a9
Hauke Witte <hw@skalar.no>
2020-02-08 13:59:10 +0100

    reverted foil overlay

--- frontend/src/components/Elements/CardListItem/CardImage.jsx
+++ frontend/src/components/Elements/CardListItem/CardImage.jsx
@@ -4,37 +4,11 @@ import styled from 'styled-components';
 
 const ANIMATION_TIME = 200;
 
-const opacity = 0.3;
-
-const StyledCard = styled.div`
-  height: ${({ isLarge }) => (isLarge ? '280' : '36')}px;
+const StyledCard = styled.img`
+  height: auto;
   width: 26px;
   transition: all ${ANIMATION_TIME}ms;
   z-index: 5;
-
-  ${({ isLarge, img }) => {
-    const multiplier = isLarge ? 10 : 1;
-    return `
-    background: repeating-linear-gradient(
-      -45deg,
-      rgba(255, 0, 0, ${opacity}) 0,
-      rgba(255, 125, 0, ${opacity}) ${5 * multiplier}px,
-      rgba(125, 125, 0, ${opacity}) ${10 * multiplier}px,
-      rgba(125, 255, 0, ${opacity}) ${15 * multiplier}px,
-      rgba(0, 255, 0, ${opacity}) ${20 * multiplier}px,
-      rgba(0, 0, 255, ${opacity}) ${25 * multiplier}px,
-      rgba(125, 125, 255, ${opacity}) ${30 * multiplier}px,
-      rgba(255, 0, 255, ${opacity}) ${35 * multiplier}px,
-      rgba(255, 0, 0, ${opacity}) ${40 * multiplier}px
-    ),
-    url(${img});
-  `;
-  }}
-
-  background-blend-mode: overlay;
-  background-position: center center;
-  background-repeat: no-repeat;
-  background-size: contain;
 
   ${({ isLarge }) => {
     if (!isLarge) return '';
@@ -80,7 +54,7 @@ export default ({ card, isOpen }) => {
   return (
     <>
       <StyledCard
-        img={images[0][showLargeImage ? 'normal' : 'small']}
+        src={images[0][showLargeImage ? 'normal' : 'small']}
         isLarge={isOpen}
         onClick={onChangeIsOpen(true)}
       />
