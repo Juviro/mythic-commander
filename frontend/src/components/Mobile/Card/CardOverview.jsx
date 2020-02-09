7952a00b950433f9e8bfb375385795379de090ab
Hauke Witte <hw@skalar.no>
2020-02-09 02:06:05 +0100

    added lazy load card

--- frontend/src/components/Mobile/Card/CardOverview.jsx
+++ frontend/src/components/Mobile/Card/CardOverview.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const StyledCardImage = styled.img`
-  width: 90%;
-  height: auto;
-  margin-top: 16px;
-`;
+import LazyLoadCard from '../../Elements/LazyLoadCard/LazyLoadCard';
 
 export default ({ card }) => {
-  const imgSrc = (card.image_uris || card.card_faces[0].image_uris).normal;
-
   return (
     <>
-      <StyledCardImage src={imgSrc} />
+      <LazyLoadCard card={card} />
     </>
   );
 };
