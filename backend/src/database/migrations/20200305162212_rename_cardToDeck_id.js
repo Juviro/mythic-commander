0d8465ec43e74807c399469a2e68cf150eda0191
Hauke Witte <hw@skalar.no>
2020-03-12 17:42:10 +0100

    fixed migration

--- backend/src/database/migrations/20200305162212_rename_cardToDeck_id.js
+++ backend/src/database/migrations/20200305162212_rename_cardToDeck_id.js
@@ -1,6 +1,7 @@
 export const up = async knex => {
   await knex.schema.alterTable('cardToDeck', table => {
     table.renameColumn('cardId', 'id');
+    table.dropPrimary();
     table.primary(['deckId', 'id']);
   });
 };
@@ -8,6 +9,7 @@ export const up = async knex => {
 export const down = async knex => {
   await knex.schema.alterTable('cardToDeck', table => {
     table.renameColumn('id', 'cardId');
+    table.dropPrimary();
     table.primary(['deckId', 'cardId']);
   });
 };
