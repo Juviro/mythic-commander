d3a833533a37dc75c302266b0e54fa1a9e5b1f98
Hauke Witte <hw@skalar.no>
2020-03-18 17:21:51 +0100

    small fix

--- frontend/src/components/Mobile/Card/CardModal.jsx
+++ frontend/src/components/Mobile/Card/CardModal.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import { Modal } from 'antd';
-import { useParams, withRouter } from 'react-router';
+import { useParams } from 'react-router';
 import Card from '.';
 
-const CardModal = ({ history }) => {
+export default () => {
   const { oracle_id } = useParams();
   if (!oracle_id) return null;
-
-  const onClose = () => {
-    const match = history.location.pathname.match(/\/m\/([-a-z]+)/);
-    const basePath = match ? match[1] : 'cards';
-    history.replace(`/m/${basePath}${history.location.search}`);
-  };
 
   return (
     <Modal
@@ -20,11 +14,8 @@ const CardModal = ({ history }) => {
       closeIcon={<div />}
       bodyStyle={{ padding: 0 }}
       className="fullscreen-modal"
-      onCancel={onClose}
     >
       <Card />
     </Modal>
   );
 };
-
-export default withRouter(CardModal);
