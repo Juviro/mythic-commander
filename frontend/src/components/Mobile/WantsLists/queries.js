c1f6ea4bd4ae25af78c3a7d892d1d9a859a6152d
Hauke Witte <hw@skalar.no>
2020-03-19 23:29:18 +0100

    add number of cards to wants list and deck overview

--- frontend/src/components/Mobile/WantsLists/queries.js
+++ frontend/src/components/Mobile/WantsLists/queries.js
@@ -6,6 +6,7 @@ export const wantsLists = gql`
       id
       name
       lastEdit
+      numberOfCards
     }
   }
 `;
@@ -16,6 +17,7 @@ export const createWantsList = gql`
       id
       name
       lastEdit
+      numberOfCards
     }
   }
 `;
