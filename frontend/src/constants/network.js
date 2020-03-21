35f12b7b1b701798ff9ae06fcdf07bfef68fc2cc
Hauke Witte <hw@skalar.no>
2020-03-22 00:57:44 +0100

    small fix

--- frontend/src/constants/network.js
+++ frontend/src/constants/network.js
@@ -2,4 +2,4 @@ const IS_DEV = process.env.NODE_ENV === 'development';
 
 export const SERVER_URL = IS_DEV
   ? 'http://localhost:4000/mtg-api/graphql'
-  : 'http://ec2-54-93-181-80.eu-central-1.compute.amazonaws.com/mtg-api/graphql';
+  : 'https://ec2-54-93-181-80.eu-central-1.compute.amazonaws.com/mtg-api/graphql';
