9766f70889286f1b1249dbb774f68bc85c54158e
Hauke Witte <hw@skalar.no>
2020-03-21 15:12:29 +0100

    updated db credentials

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -16,11 +16,24 @@ module.exports = {
     connection: {
       user: 'hauke',
       database: 'mtg',
-      password: 'dqwfhife83hfwkrh38fnwfbcxsi3r2uu9fcewrb321f90d8fy2b3r98dsfye9vhdqww83rnfY',
+      password: '17ARqkNuJWKgyDHAEVDP',
       host: 'localhost',
     },
     migrations: {
       directory: './src/database/migrations',
     },
   },
+  // production: {
+  //   client: 'pg',
+  //   connection: {
+  //     user: 'hauke',
+  //     database: 'mtg',
+  //     password:
+  //       'dqwfhife83hfwkrh38fnwfbcxsi3r2uu9fcewrb321f90d8fy2b3r98dsfye9vhdqww83rnfY',
+  //     host: 'localhost',
+  //   },
+  //   migrations: {
+  //     directory: './src/database/migrations',
+  //   },
+  // },
 };
