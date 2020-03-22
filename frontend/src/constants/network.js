e59e0ed058813287237a3ddd4637069263e490a6
Hauke Witte <hw@skalar.no>
2020-03-22 13:36:35 +0100

    update deploy script

--- frontend/src/constants/network.js
+++ frontend/src/constants/network.js
@@ -2,4 +2,4 @@ const IS_DEV = process.env.NODE_ENV === 'development';
 
 export const SERVER_URL = IS_DEV
   ? 'http://localhost:4000/mtg-api/graphql'
-  : 'https://mythic-commander.com/mtg-api/graphql';
+  : '/mtg-api/graphql';
---
+++ scripts/deploy-prod.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo -e "\e[44mBuilding frontend\e[0m"
+cd /opt/mtg/frontend && yarn install --production=false && rm -rf /opt/mtg/frontend/build/* && yarn build
+
+echo -e "\e[44mBuilding backend\e[0m"
+cd /opt/mtg/backend && yarn install --production=false && yarn build
+
+echo -e "\e[44mCopying schema\e[0m"
+cp /opt/mtg/backend/src/graphql/schema.graphql /opt/mtg/backend/dist/graphql/schema.graphql
+
+echo -e "\e[44mMigrating Database\e[0m"
+yarn migrate
+
+echo -e "\e[44mrestarting backend\e[0m"
+pm2 restart backend
