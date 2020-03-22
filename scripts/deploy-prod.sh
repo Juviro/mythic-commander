0e51f51290d840c78d4b2b5b1262eb5b0c99b127
Hauke Witte <hw@skalar.no>
2020-03-22 14:05:04 +0100

    small fix

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -11,7 +11,11 @@ echo -e "\e[44mCopying schema\e[0m"
 cp /opt/mtg/backend/src/graphql/schema.graphql /opt/mtg/backend/dist/graphql/schema.graphql
 
 echo -e "\e[44mMigrating Database\e[0m"
-yarn migrate
+NODE_ENV=production yarn migrate
+
+echo -e "\e[44mdeploying frontend\e[0m"
+rm -rf /var/www/mtg/* 
+cp -r /opt/mtg/frontend/build/* /var/www/mtg/
 
 echo -e "\e[44mrestarting backend\e[0m"
 NODE_ENV=production pm2 restart backend
