587510080279b6ae1d918e79814240f648ecc56e
Hauke Witte <hw@skalar.no>
2020-03-22 13:36:35 +0100

    update deploy script

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -11,6 +11,19 @@ module.exports = {
     },
   },
 
+  staging: {
+    client: 'pg',
+    connection: {
+      user: 'hauke',
+      database: 'mtg',
+      password:
+        'dqwfhife83hfwkrh38fnwfbcxsi3r2uu9fcewrb321f90d8fy2b3r98dsfye9vhdqww83rnfY',
+      host: 'localhost',
+    },
+    migrations: {
+      directory: './src/database/migrations',
+    },
+  },
   production: {
     client: 'pg',
     connection: {
@@ -23,17 +36,4 @@ module.exports = {
       directory: './src/database/migrations',
     },
   },
-  // production: {
-  //   client: 'pg',
-  //   connection: {
-  //     user: 'hauke',
-  //     database: 'mtg',
-  //     password:
-  //       'dqwfhife83hfwkrh38fnwfbcxsi3r2uu9fcewrb321f90d8fy2b3r98dsfye9vhdqww83rnfY',
-  //     host: 'localhost',
-  //   },
-  //   migrations: {
-  //     directory: './src/database/migrations',
-  //   },
-  // },
 };
