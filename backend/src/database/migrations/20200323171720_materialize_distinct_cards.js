d0403bf2259068795e265c1d0385720ad7d456bc
Hauke Witte <hw@skalar.no>
2020-03-23 17:23:17 +0100

    change distinct cards to materialized viw

---
+++ backend/src/database/migrations/20200323171720_materialize_distinct_cards.js
@@ -0,0 +1,29 @@
+export const up = async knex => {
+  await knex.schema.raw(`
+            DROP VIEW "distinctCards"
+          `);
+  await knex.schema.raw(`
+        CREATE MATERIALIZED VIEW "distinctCards" AS 
+        WITH _cards AS (
+            SELECT
+            *,
+            row_number() OVER (PARTITION BY name ORDER BY (prices->>'usd')::float ASC, frame DESC, released_at DESC) AS row_number
+            FROM cards
+            WHERE 'paper' = ANY(games) 
+            AND layout <> ALL ( ARRAY[
+                'token', 
+                'double_faced_token', 
+                'emblem',
+                'planar',
+                'vanguard',
+                'scheme'
+            ])
+        )
+        SELECT *
+        FROM _cards
+        WHERE row_number = 1;
+    `);
+};
+
+// eslint-disable-next-line
+export const down = () => {};
