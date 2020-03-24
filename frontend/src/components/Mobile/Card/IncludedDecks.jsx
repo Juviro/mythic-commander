6bdada3b04589979070c0b64d02f27d90732b363
Hauke Witte <hw@skalar.no>
2020-03-24 16:33:56 +0100

    add deck to card details

---
+++ frontend/src/components/Mobile/Card/IncludedDecks.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { List, Typography } from 'antd';
+import { Link } from 'react-router-dom';
+
+import styled from 'styled-components';
+import { CustomSkeleton } from '../../Elements';
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const StyledPreview = styled.img`
+  width: 38px;
+  height: auto;
+  margin-right: 8px;
+  border-radius: 2px;
+`;
+
+export default ({ card }) => {
+  if (!card) return <CustomSkeleton.Line />;
+
+  const { containingDecks } = card;
+
+  if (!containingDecks.length)
+    return (
+      <Typography.Text type="secondary">
+        This card is currently in none of your decks
+      </Typography.Text>
+    );
+
+  const dataSource = containingDecks.sort((a, b) => (a.name > b.name ? 1 : -1));
+
+  return (
+    <StyledWrapper>
+      <List
+        size="small"
+        dataSource={dataSource}
+        renderItem={({ id, name, imgSrc }) => (
+          <List.Item style={{ padding: 4 }}>
+            <span>
+              <StyledPreview src={imgSrc} />
+              <Link to={`/m/decks/${id}`}>{name}</Link>
+            </span>
+          </List.Item>
+        )}
+      />
+    </StyledWrapper>
+  );
+};
