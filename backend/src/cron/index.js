ceaac39c28dc7e7dad510ef03246d2b5e9aab1e7
Hauke Witte <hw@skalar.no>
2020-03-24 15:04:47 +0100

    add collection value and amount tables and cronjobs

--- backend/src/cron/index.js
+++ backend/src/cron/index.js
@@ -1,10 +1,13 @@
 import { CronJob } from 'cron';
+
 import updateCards from './updateCards';
 import deleteSessions from './deleteSessions';
+import collectionValue from './collectionValue';
 
 const startCronjobs = () => {
-  new CronJob('0 0 21 * * *', updateCards, null, true, 'Europe/Berlin');
   new CronJob('0 0 20 * * *', deleteSessions, null, true, 'Europe/Berlin');
+  new CronJob('0 0 21 * * *', updateCards, null, true, 'Europe/Berlin');
+  new CronJob('0 0 22 * * *', collectionValue, null, true, 'Europe/Berlin');
 };
 
 startCronjobs();
