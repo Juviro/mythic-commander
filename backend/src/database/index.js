35b77ea3f308e1861003f57180f2cc4bac531261
Hauke Witte <hw@skalar.no>
2020-03-27 14:32:50 +0100

    small fix

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -2,10 +2,7 @@ import knex from 'knex';
 import knexfile from '../../knexfile';
 
 const env = process.env.NODE_ENV || 'development';
-console.log(' process.env.NODE_ENV :', process.env.NODE_ENV);
-console.log('env :', env);
 const configOptions = knexfile[env];
-console.log('configOptions :', configOptions);
 
 const db = knex(configOptions);
 
