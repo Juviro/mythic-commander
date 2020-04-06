c74e376f1470457fa811083a17cb1131e2206e09
Hauke Witte <hw@skalar.no>
2020-04-06 17:53:06 +0200

    remove parallax

--- frontend/src/components/Mobile/Deck/DeckHeader/DeckImage.jsx
+++ frontend/src/components/Mobile/Deck/DeckHeader/DeckImage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Affix, Spin } from 'antd';
 import styled from 'styled-components';
 import { Parallax } from 'react-parallax';
@@ -6,32 +6,31 @@ import { useWindowSize } from '../../../Hooks';
 
 const StyledAffix = styled(Affix)`
   width: 100%;
-  display: flex;
-  flex-direction: column;
-`;
-
-const StyledWrapper = styled.div`
+  height: calc(20vh + 20vw);
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
+`;
+
+const StyledImage = styled.div`
+  width: 100vw;
   height: calc(20vh + 20vw);
   border-bottom: 1px solid #f1f1f1;
+
+  background-image: url(${({ img }) => img});
+  background-size: cover;
+  background-repeat: no-repeat;
 `;
 
 export default ({ imgSrc, loading }) => {
-  const imageHeight = window.innerHeight * 0.2 + window.innerWidth * 0.2;
-  useWindowSize();
+  const imageHeight = window.innerWidth * 0.2 + window.innerHeight * 0.2;
 
+  useWindowSize();
   return (
     <>
       <StyledAffix offsetTop={50 - imageHeight}>
-        <Parallax
-          strength={400}
-          bgImage={imgSrc}
-          bgImageStyle={{ width: '100%', height: 'auto', top: 50 }}
-        >
-          <StyledWrapper>{loading && <Spin />}</StyledWrapper>
-        </Parallax>
+        {loading ? <Spin /> : <StyledImage img={imgSrc} />}
       </StyledAffix>
     </>
   );
