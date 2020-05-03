791b9fcaf5917ed13c636d70e1d5638d2beacce8
Hauke Witte <hw@skalar.no>
2020-05-03 20:31:43 +0200

    fix adding card multiple times to deck

--- backend/src/graphql/resolvers/mutations/deck/addCardsToDeck.js
+++ backend/src/graphql/resolvers/mutations/deck/addCardsToDeck.js
@@ -1,31 +1,25 @@
 import { updateLastEdit } from './helper';
 import { canAccessDeck } from '../../../../auth/authenticateUser';
 
+const ON_CONFLICT = `
+    ON CONFLICT (id, "deckId") 
+    DO UPDATE SET 
+      amount = "cardToDeck".amount + EXCLUDED.amount, 
+      "createdAt" = NOW()
+  `;
+
 export default async (_, { cards, deckId }, { user, db }) => {
   await canAccessDeck(user.id, deckId);
 
-  const { rows: cardsAlreadyInDeck } = await db.raw(
-    `
-    SELECT cards.id 
-    FROM cards 
-    LEFT JOIN "cardToDeckWithOracle" 
-      ON "cardToDeckWithOracle".oracle_id = cards.oracle_id 
-    WHERE cards.id = ANY(?)
-      AND "deckId" = ?;
-  `,
-    [cards.map(({ id }) => id), deckId]
-  );
+  const cardsToInsert = cards.map(({ id, amount = 1 }) => ({
+    id,
+    amount,
+    deckId,
+  }));
 
-  const cardIdsAlreadyInDeck = cardsAlreadyInDeck.map(({ id }) => id);
-  const cardsToInsert = cards
-    .filter(({ id }) => !cardIdsAlreadyInDeck.includes(id))
-    .map(({ id, amount = 1 }) => ({
-      id,
-      amount,
-      deckId,
-    }));
+  const query = db('cardToDeck').insert(cardsToInsert);
 
-  await db('cardToDeck').insert(cardsToInsert);
+  await db.raw(query + ON_CONFLICT);
 
   await updateLastEdit(deckId, db);
 
