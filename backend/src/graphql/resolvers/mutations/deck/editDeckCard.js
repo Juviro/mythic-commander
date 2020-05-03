83128a07298828c4aa8ee145c022b0c2e686dc5a
Hauke Witte <hw@skalar.no>
2020-05-03 20:34:27 +0200

    small fix

--- backend/src/graphql/resolvers/mutations/deck/editDeckCard.js
+++ backend/src/graphql/resolvers/mutations/deck/editDeckCard.js
@@ -5,15 +5,19 @@ import unifyCardFormat from '../../unifyCardFormat';
 export default async (_, { cardId, deckId, newProps }, { user, db }) => {
   await canAccessDeck(user.id, deckId);
 
-  await db('cardToDeck')
-    .where({ id: cardId, deckId })
-    .update(newProps);
-  await updateLastEdit(deckId, db);
+  try {
+    await db('cardToDeck')
+      .where({ id: cardId, deckId })
+      .update(newProps);
+    await updateLastEdit(deckId, db);
 
-  const newCardId = newProps.id || cardId;
-  const [updatedCard] = await db('cardToDeck')
-    .leftJoin('cards', { 'cards.id': 'cardToDeck.id' })
-    .where({ deckId, 'cardToDeck.id': newCardId });
+    const newCardId = newProps.id || cardId;
+    const [updatedCard] = await db('cardToDeck')
+      .leftJoin('cards', { 'cards.id': 'cardToDeck.id' })
+      .where({ deckId, 'cardToDeck.id': newCardId });
 
-  return unifyCardFormat(deckId)(updatedCard);
+    return unifyCardFormat(deckId)(updatedCard);
+  } catch {
+    throw new Error('That card edition already exists in this deck');
+  }
 };
