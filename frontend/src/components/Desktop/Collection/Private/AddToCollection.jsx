bb5aa6d7f2e45eefddebb07356c5d266d588029b
juviro <hauketw@gmail.com>
2020-06-22 21:00:18 +0200

    improve advanced search speed

--- frontend/src/components/Desktop/Collection/Private/AddToCollection.jsx
+++ frontend/src/components/Desktop/Collection/Private/AddToCollection.jsx
@@ -13,28 +13,29 @@ export default () => {
     message(`Added <b>${addedName}</b> to your collection!`);
     mutate({
       variables: { cards },
-      update: (cache, { data: updateData }) => {
-        if (!updateData) return;
-        const { addToCollection: newCards } = updateData;
-        const existing = cache.readQuery({
-          query: getCollectionDesktop,
-        });
+      refetchQueries: ['getCollectionDesktop'],
+      // update: (cache, { data: updateData }) => {
+      //   if (!updateData) return;
+      //   const { addToCollection: newCards } = updateData;
+      //   const existing = cache.readQuery({
+      //     query: getCollectionDesktop,
+      //   });
 
-        const existingCards = existing.collection.cards.filter(
-          ({ card: { oracle_id } }) =>
-            !newCards.some(({ card }) => card.oracle_id === oracle_id)
-        );
+      //   const existingCards = existing.collection.cards.filter(
+      //     ({ card: { oracle_id } }) =>
+      //       !newCards.some(({ card }) => card.oracle_id === oracle_id)
+      //   );
 
-        cache.writeQuery({
-          query: getCollectionDesktop,
-          data: {
-            collection: {
-              ...existing.collection,
-              cards: [...newCards, ...existingCards],
-            },
-          },
-        });
-      },
+      //   cache.writeQuery({
+      //     query: getCollectionDesktop,
+      //     data: {
+      //       collection: {
+      //         ...existing.collection,
+      //         cards: [...newCards, ...existingCards],
+      //       },
+      //     },
+      //   });
+      // },
     });
   };
 
--- frontend/src/components/Elements/Desktop/PaginatedCardList/CardTable/useTableShortcuts.jsx
+++ frontend/src/components/Elements/Desktop/PaginatedCardList/CardTable/useTableShortcuts.jsx
@@ -99,7 +99,7 @@ export default (numberOfCards, toggleShowDetail, toggleElementSelection) => {
     current: currentPage,
     total: numberOfCards,
     onShowSizeChange: (_, newPageSize) => setPageSize(newPageSize),
-    pageSizeOptions: [10, 25, 50, 100],
+    pageSizeOptions: [10, 25, 50],
     onChange: val => setCurrentPage(val),
   };
 
