bf013f00a2ebd7f1f226a4a1cbff32873a704eb0
juviro <hauketw@gmail.com>
2020-07-03 21:47:14 +0200

    small fix

--- frontend/src/components/Mobile/Card/CardOwned/OwnedOverview.jsx
+++ frontend/src/components/Mobile/Card/CardOwned/OwnedOverview.jsx
@@ -55,7 +55,7 @@ export default ({ cardOracleId, cards, onChangeSet, selectedCardId }) => {
           variables: { oracle_id: cardOracleId },
         },
 
-        'getCollectionNames',
+        'ownedCardNames',
       ],
     });
     message.success('Updated your collection!');
