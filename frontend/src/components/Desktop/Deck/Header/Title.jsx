8f43a83d9968e7d222d54938fcca3d01bcf4aa3f
Hauke Witte <hauketw@gmail.com>
2020-07-24 23:14:57 +0200

    small fix

--- frontend/src/components/Desktop/Deck/Header/Title.jsx
+++ frontend/src/components/Desktop/Deck/Header/Title.jsx
@@ -5,9 +5,7 @@ import { Flex, DeckTitle, ColorIdentitySymbols } from '../../../Elements/Shared'
 export default ({ deck }) => {
   return (
     <Flex direction="row" align="center">
-      <Flex style={{ marginRight: 8 }}>
-        <ColorIdentitySymbols cards={deck.cards} size={24} />
-      </Flex>
+      <ColorIdentitySymbols cards={deck.cards} size={24} />
       <DeckTitle deck={deck} />
     </Flex>
   );
