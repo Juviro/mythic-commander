0b96c83efd0e461318a13da844d3b31bf2243ef8
Hauke Witte <hauketw@gmail.com>
2020-07-25 00:14:40 +0200

    refactor: improve deck card loading speed

--- backend/src/graphql/resolvers/queries/Deck.js
+++ backend/src/graphql/resolvers/queries/Deck.js
@@ -1,10 +1,29 @@
 import unifyCardFormat from '../unifyCardFormat';
 
 const resolver = {
-  async cards({ id: deckId }, _, { db }) {
-    const cards = await db('cardToDeck')
-      .leftJoin('cards', { 'cards.id': 'cardToDeck.id' })
-      .where({ deckId });
+  async cards({ id: deckId }, _, { db, user: { id: userId } }) {
+    const { rows: cards } = await db.raw(
+      `
+      WITH "ownedOracleCards" AS (
+        SELECT
+        true as owned,
+        oracle_id,
+        row_number() OVER (PARTITION BY oracle_id) AS row_number
+        FROM "collectionWithOracle"
+        WHERE "userId"=?
+        )
+        select 
+        CASE WHEN owned = TRUE THEN TRUE else FALSE END as owned, 
+        "cardToDeckWithOracle".*, 
+        cards.*  from "cardToDeckWithOracle" 
+        LEFT JOIN "cards" 
+          ON "cards"."id" = "cardToDeckWithOracle"."id" 
+        LEFT JOIN "ownedOracleCards" 
+          ON "ownedOracleCards"."oracle_id" = "cardToDeckWithOracle"."oracle_id" 
+        WHERE  row_number = 1 AND "deckId" = ?;
+        `,
+      [userId, deckId]
+    );
 
     return cards.map(unifyCardFormat(deckId));
   },
