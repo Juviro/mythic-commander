55e44598e97045249514793affaae92cb1f9ef3d
Hauke Witte <hauketw@gmail.com>
2020-08-07 18:31:28 +0200

    wip

--- frontend/src/components/Mobile/LifeTracker/GameScreen/PlayerField/PlayerField.jsx
+++ frontend/src/components/Mobile/LifeTracker/GameScreen/PlayerField/PlayerField.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
-import { Flex } from '../../../../Elements/Shared';
-import NumberField from '../../NumberField/NumberField';
+import styled from 'styled-components';
 
-export default ({ player, onSetLife }) => {
+import PlayerName from './PlayerName';
+import NumberField from '../../NumberField';
+import { Flex } from '../../../../Elements/Shared';
+import { lifeTrack } from '../../../../../constants/colors';
+
+const StyledWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  height: 100%;
+  border: 1px solid black;
+  background-color: ${({ playerIndex }) => lifeTrack[playerIndex]};
+
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url(https://cdnb.artstation.com/p/assets/images/images/027/681/525/large/paul-canavan-414596-felineincarnations-full.jpg?1592245465);
+`;
+
+export default ({ player, players, onSetLife, onChangeName }) => {
+  const playerIndex = players.findIndex(({ id }) => id === player.id);
   return (
-    <Flex
-      direction="row"
-      align="center"
-      justify="center"
-      style={{
-        flex: 1,
-        height: '100%',
-        border: '1px solid black',
-        backgroundColor: 'white',
-      }}
-    >
-      <NumberField value={player.life} setValue={val => onSetLife(player.id, val)} />
-    </Flex>
+    <StyledWrapper playerIndex={playerIndex}>
+      <NumberField
+        whiteText
+        value={player.life}
+        setValue={val => onSetLife(player.id, val)}
+      />
+      <PlayerName player={player} onChangeName={onChangeName} />
+    </StyledWrapper>
   );
 };
--- frontend/src/components/Mobile/LifeTracker/GameScreen/PlayerField/PlayerName/PlayerName.jsx
+++ frontend/src/components/Mobile/LifeTracker/GameScreen/PlayerField/PlayerName/PlayerName.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default ({ name, onChangeName }) => {
-  return null;
+import EditName from './EditName';
+import { useToggle } from '../../../../../Hooks';
+
+const StyledName = styled.div`
+  font-size: 200%;
+  color: #fff;
+  width: 100%;
+  text-align: center;
+  text-shadow: 2px 2px 0px #333;
+`;
+
+export default ({ player, onChangeName }) => {
+  // TODO: false
+  const [isEditing, toggleIsEditing] = useToggle(player.name === 'Player 1');
+
+  const onSubmit = val => {
+    console.log('val :', val);
+  };
+
+  return (
+    <>
+      <StyledName onClick={toggleIsEditing}>{player.name}</StyledName>
+      <EditName
+        visible={isEditing}
+        onClose={toggleIsEditing}
+        onSubmit={onSubmit}
+        player={player}
+      />
+    </>
+  );
 };
