019fa2714109e9375f761c15cc47ce9300b19c5b
Hauke Witte <hauketw@gmail.com>
2020-08-08 21:34:35 +0200

    fix: delete backend build folder

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -5,7 +5,7 @@ echo -e "\e[44mBuilding frontend\e[0m"
 cd /opt/mtg/frontend && yarn install --production=false && rm -rf /opt/mtg/frontend/build/* && ./node_modules/.bin/react-scripts --max_old_space_size=4096 build
 
 echo -e "\e[44mBuilding backend\e[0m"
-cd /opt/mtg/backend && yarn install --production=false && yarn build
+cd /opt/mtg/backend && rm -rf /opt/mtg/backend/dist/* && yarn install --production=false && yarn build
 
 echo -e "\e[44mCopying schema\e[0m"
 cp /opt/mtg/backend/src/graphql/schema.graphql /opt/mtg/backend/dist/graphql/schema.graphql
