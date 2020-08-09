713c5b15bf3f85805f068ece2261cf557efd6ff1
Hauke Witte <hauketw@gmail.com>
2020-08-09 11:09:20 +0200

    small fix

--- frontend/src/components/Elements/Desktop/AddCards/AdvancedSearch/AdvancedSearch.jsx
+++ frontend/src/components/Elements/Desktop/AddCards/AdvancedSearch/AdvancedSearch.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import { Checkbox } from 'antd';
+import React, { useState, useEffect } from 'react';
 
+import styled from 'styled-components';
 import { Flex, Expander } from '../../../Shared';
 import { useToggle } from '../../../../Hooks';
 import SearchSettings from './SearchSettings';
 import MultiInput from '../MultIinput';
 
+const ANIMATION_DURATION = 300;
+
+const StyledLink = styled.a`
+  margin-top: 8px;
+`;
+
 export default ({ setCardOptions, onAddCards }) => {
   const [isExpanded, toggleExpanded] = useToggle();
+  const [linkText, setLinkText] = useState('advanced...');
+
+  useEffect(() => {
+    const newLinkText = isExpanded ? 'show less' : 'advanced...';
+    setTimeout(() => setLinkText(newLinkText), ANIMATION_DURATION);
+  }, [isExpanded]);
 
   return (
     <Flex direction="column">
-      <Checkbox checked={isExpanded} onChange={toggleExpanded} style={{ marginTop: 8 }}>
-        Advanced
-      </Checkbox>
+      <StyledLink type="link" onClick={toggleExpanded}>
+        {linkText}
+      </StyledLink>
       <Expander isExpanded={isExpanded}>
         <Flex direction="column">
           <SearchSettings setCardOptions={setCardOptions} />
