ebb398e867048c2a87bd7a5b1398e0bd666e5fea
Hauke Witte <hauketw@gmail.com>
2020-08-11 21:28:56 +0200

    small fix

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -4,7 +4,7 @@ export const images = [
   'https://img.scryfall.com/cards/art_crop/front/7/8/780136cb-438d-4679-96eb-807da6faa157.jpg', // Forest
   'https://img.scryfall.com/cards/art_crop/front/b/6/b6566108-ccc7-4fac-b935-1eb212889543.jpg', // Swamp 2
   'https://img.scryfall.com/cards/art_crop/front/8/a/8a97cc92-6894-4c6f-8c8d-bfc9fdd4f974.jpg', // Swamp
-  'https://img.scryfall.com/cards/art_crop/front/0/5/05bc1b45-ffb8-4efa-8ba8-8601ff38ed62.jpg', // Island
+  'https://img.scryfall.com/cards/art_crop/front/7/8/78732e7f-421d-43e8-8f43-341bb01e993e.jpg', // Island
   'https://img.scryfall.com/cards/art_crop/front/1/a/1a8d9be0-255a-482a-b055-f483859266c5.jpg', // Mountain
   'https://img.scryfall.com/cards/art_crop/front/3/7/37827425-f304-43eb-979b-1ec5b923a2b5.jpg', // Mountain 2
   'https://img.scryfall.com/cards/art_crop/front/7/a/7a2c8b8e-2e28-4f10-b04f-9b313c60c0bb.jpg', // Plains
