02959e67c8b14f0d79a97d627d2f167396da5b05
Hauke Witte <hauketw@gmail.com>
2020-08-13 17:35:54 +0200

    small fix

--- frontend/src/components/Mobile/LifeTracker/GameScreen/GameScreen.jsx
+++ frontend/src/components/Mobile/LifeTracker/GameScreen/GameScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
+import { useBeforeunload } from 'react-beforeunload';
 
 import useGameState from './useGameState';
 import GameField from './GameField';
@@ -8,6 +9,8 @@ import Menu from './Menu';
 
 const GameScreen = ({ gameSettings, history, handle, displayDamage }) => {
   const isValidGame = Boolean(gameSettings.numberOfPlayers);
+
+  useBeforeunload(() => 'Are you sure you want to leave the game?');
 
   const {
     players,
