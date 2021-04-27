f37ca056728f67239589317747152e5e41c437bf
Witte Hauke <hauke.witte@viu.ch>
2021-04-27 16:51:33 +0200

    feat (multiinput): allow filtering by set

--- frontend/src/components/Elements/Desktop/AddCards/MultIinput/MultiInput.jsx
+++ frontend/src/components/Elements/Desktop/AddCards/MultIinput/MultiInput.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Input, Tooltip, Button, Modal, message } from 'antd';
+import { Input, Tooltip, Button, Modal, message, Space, Typography } from 'antd';
 import {
   CheckCircleOutlined,
   ExclamationCircleOutlined,
   ImportOutlined,
 } from '@ant-design/icons';
 
+import SearchSettings from 'components/Elements/Desktop/AddCards/AdvancedSearch/SearchSettings';
 import {
   StyledWrapper,
   StyledInputWrapper,
@@ -34,6 +35,7 @@ export default class MultiInput extends React.Component {
     isOpen: false,
     cardResults: [],
     isValidInput: false,
+    cardOptions: null,
   };
 
   static contextType = CardContext;
@@ -41,8 +43,12 @@ export default class MultiInput extends React.Component {
   componentDidUpdate(_, prevState) {
     // checks for rows that need to be updated
     const { cardNames } = this.context;
-    const { value, cardResults } = this.state;
+    const { value, cardResults, cardOptions } = this.state;
+
+    const currentCardNames = cardOptions?.cards?.map(({ name }) => name) ?? cardNames;
+
     const oldValues = prevState.value.split('\n');
+    const forceUpdate = prevState.cardOptions !== cardOptions;
     let shouldUpdate = false;
     // prevents bug with pressing enter in an empty field
     if (!prevState.value.replace(/[\s]+/g, '') && !value.replace(/[\s]+/g, '')) {
@@ -50,9 +56,14 @@ export default class MultiInput extends React.Component {
     }
 
     const newCardResult = value.split('\n').map((row, index) => {
-      if (row === oldValues[index]) return cardResults[index] || '';
+      if (row === oldValues[index] && !forceUpdate) return cardResults[index] || '';
       shouldUpdate = true;
-      if (!row.replace(/[\s]+/g, '') || row.startsWith('//') || row.startsWith('SB')) {
+      if (
+        !row.replace(/[\s]+/g, '') ||
+        row.startsWith('//') ||
+        row.startsWith('SB') ||
+        row.match(/\(\d+\)$/)
+      ) {
         return { name: NO_CARD };
       }
       // Removes sideboard indicators (currently filtered by the function above)
@@ -61,7 +72,7 @@ export default class MultiInput extends React.Component {
       const normalize = (str) =>
         str.toLowerCase().replace(/(^[sb:\s]*[\d]+x*|\/\/.+$|[.,'\s]+)/g, '');
       const normalizedRow = normalize(name);
-      const foundName = cardNames.find(
+      const foundName = currentCardNames.find(
         (cardName) => normalize(cardName) === normalizedRow
       );
       return { name: foundName, amount };
@@ -82,7 +93,7 @@ export default class MultiInput extends React.Component {
 
   onSubmit = () => {
     const { onAddCards } = this.props;
-    const { cardResults } = this.state;
+    const { cardResults, cardOptions } = this.state;
     const { cards } = this.context;
     if (!cards || !cards.length) {
       message.warn('Still initializing.. please try again');
@@ -90,9 +101,16 @@ export default class MultiInput extends React.Component {
     }
 
     const cardNames = cardResults.filter(({ name }) => name && name !== NO_CARD);
+
+    const getIdByName = (name) => {
+      if (cardOptions?.cards)
+        return cardOptions.cards.find((card) => card.name === name)?.id;
+      return cards.find((card) => card.name === name)?.id;
+    };
+
     const cardIds = cardNames.map(({ name, amount }) => ({
       amount,
-      id: cards.find((card) => card.name === name).id,
+      id: getIdByName(name),
     }));
     const uniqueCardIds = cardIds.filter(
       ({ id }, index) => cardIds.findIndex((cardId) => cardId.id === id) === index
@@ -121,7 +139,7 @@ export default class MultiInput extends React.Component {
         <Modal
           destroyOnClose
           visible={isOpen}
-          title="Import Multiple"
+          title="Import Card List"
           onCancel={() => this.setState({ isOpen: false })}
           footer={
             <Button
@@ -134,39 +152,47 @@ export default class MultiInput extends React.Component {
           }
         >
           <StyledWrapper>
-            <StyledInputWrapper>
-              <Input.TextArea
-                value={value}
-                autoFocus
-                onChange={this.onChange}
-                autoSize={{ minRows: 9 }}
-                style={{ whiteSpace: 'pre' }}
-                onPressEnter={(e) => {
-                  const isSubmit = e.metaKey || e.ctrlKey;
-                  if (isSubmit && isValidInput) this.onSubmit();
-                }}
-                placeholder={PLACEHOLDER}
-              />
-              <StyledStatus>
-                {cardResults.map(({ name }) => (
-                  <IconWrapper key={Math.random()}>
-                    {name !== NO_CARD && (
-                      <Tooltip placement="right" title={name || 'Could not find card'}>
-                        {name ? (
-                          <CheckCircleOutlined
-                            style={{ color: 'green', marginLeft: 16 }}
-                          />
-                        ) : (
-                          <ExclamationCircleOutlined
-                            style={{ color: 'red', marginLeft: 16 }}
-                          />
-                        )}
-                      </Tooltip>
-                    )}
-                  </IconWrapper>
-                ))}
-              </StyledStatus>
-            </StyledInputWrapper>
+            <Space size={24} direction="vertical">
+              <Space direction="vertical" style={{ width: '100%' }}>
+                <Typography.Text>Only cards from specific set:</Typography.Text>
+                <SearchSettings
+                  setCardOptions={(cardOptions) => this.setState({ cardOptions })}
+                />
+              </Space>
+              <StyledInputWrapper>
+                <Input.TextArea
+                  value={value}
+                  autoFocus
+                  onChange={this.onChange}
+                  autoSize={{ minRows: 9 }}
+                  style={{ whiteSpace: 'pre' }}
+                  onPressEnter={(e) => {
+                    const isSubmit = e.metaKey || e.ctrlKey;
+                    if (isSubmit && isValidInput) this.onSubmit();
+                  }}
+                  placeholder={PLACEHOLDER}
+                />
+                <StyledStatus>
+                  {cardResults.map(({ name }) => (
+                    <IconWrapper key={Math.random()}>
+                      {name !== NO_CARD && (
+                        <Tooltip placement="right" title={name || 'Could not find card'}>
+                          {name ? (
+                            <CheckCircleOutlined
+                              style={{ color: 'green', marginLeft: 16 }}
+                            />
+                          ) : (
+                            <ExclamationCircleOutlined
+                              style={{ color: 'red', marginLeft: 16 }}
+                            />
+                          )}
+                        </Tooltip>
+                      )}
+                    </IconWrapper>
+                  ))}
+                </StyledStatus>
+              </StyledInputWrapper>
+            </Space>
           </StyledWrapper>
         </Modal>
       </>
