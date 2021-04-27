0b1a772cb0d7442dd5c49b5adc5ac3d515aba9fd
Witte Hauke <hauke.witte@viu.ch>
2021-04-27 16:51:33 +0200

    feat (multiinput): allow filtering by set

--- frontend/src/components/Elements/Desktop/AddCards/AddCards.jsx
+++ frontend/src/components/Elements/Desktop/AddCards/AddCards.jsx
@@ -56,7 +56,7 @@ export default ({
       {isAdvanced && (
         <StyledSetSelectionWrapper>
           <StyledLabel>Filter by Set</StyledLabel>
-          <SearchSettings setCardOptions={setCardOptions} onAddCards={onAddCards} />
+          <SearchSettings setCardOptions={setCardOptions} />
         </StyledSetSelectionWrapper>
       )}
     </Flex>
