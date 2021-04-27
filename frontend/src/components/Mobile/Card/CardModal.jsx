30549912ad4e030baa0eff8acebde422a4a4fe1a
Witte Hauke <hauke.witte@viu.ch>
2021-04-28 00:29:46 +0200

    feat (details mobile): allow swiping to next/previous card

--- frontend/src/components/Mobile/Card/CardModal.jsx
+++ frontend/src/components/Mobile/Card/CardModal.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { Modal } from 'antd';
-
+import styled from 'styled-components';
 import { withRouter } from 'react-router';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 import Card from '.';
 
-const CardModal = ({ oracle_id, id, onClose, history }) => {
+const StyledSwiper = styled(Swiper)`
+  .swiper-wrapper {
+    display: -webkit-box;
+  }
+`;
+
+const CardModal = ({
+  oracle_id,
+  id,
+  onClose,
+  history,
+  cards,
+  setDetailCard,
+  onLoadMore,
+}) => {
   if (!oracle_id) return null;
+
+  const currentIndex = cards.findIndex((card) => card.id === id);
 
   window.onpopstate = onClose;
 
@@ -14,10 +32,33 @@ const CardModal = ({ oracle_id, id, onClose, history }) => {
       visible
       footer={null}
       onCancel={history.goBack}
-      bodyStyle={{ padding: '32px 0 0' }}
+      bodyStyle={{ padding: '32px 0 0', overflow: 'hidden' }}
       className="fullscreen-modal"
     >
-      <Card overwriteOracleId={oracle_id} defaultCardId={id} />
+      <StyledSwiper
+        spaceBetween={48}
+        initialSlide={currentIndex}
+        slidesPerView={1}
+        onSlideChange={(swiper) => {
+          setDetailCard(cards[swiper.activeIndex]);
+          if (swiper.activeIndex + 2 === cards.length) {
+            onLoadMore();
+          }
+        }}
+      >
+        {cards.map((card, index) => {
+          const isRendered = Math.abs(index - currentIndex) <= 1;
+          return (
+            <SwiperSlide key={card.oracle_id} style={{ width: '100%' }}>
+              <span>
+                {isRendered && (
+                  <Card overwriteOracleId={card.oracle_id} defaultCardId={card.id} />
+                )}
+              </span>
+            </SwiperSlide>
+          );
+        })}
+      </StyledSwiper>
     </Modal>
   );
 };
