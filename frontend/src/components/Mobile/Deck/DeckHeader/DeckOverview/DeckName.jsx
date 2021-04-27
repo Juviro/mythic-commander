e1b566aa81d5919f37b2299bd555fb5aa31bda4d
Witte Hauke <hauke.witte@viu.ch>
2021-04-27 15:42:13 +0200

    feat (deck mobile): add value label

--- frontend/src/components/Mobile/Deck/DeckHeader/DeckOverview/DeckName.jsx
+++ frontend/src/components/Mobile/Deck/DeckHeader/DeckOverview/DeckName.jsx
@@ -1,22 +1,21 @@
+import { Typography } from 'antd';
 import React from 'react';
 import styled from 'styled-components';
 import { colors } from '../../../../../assets/mtgIcons';
 
-const StyledNameWrapepr = styled.div`
+const StyledNameWrapper = styled(Typography.Title)`
   width: 100%;
   display: flex;
   flex-direction: row;
-  font-size: 16px;
   align-items: center;
-  font-weight: 600;
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
 `;
 
 const StyledColorIcon = styled.img`
-  width: 16px;
-  height: 16px;
+  width: 24px;
+  height: 24px;
   margin-left: 4px;
   border: 1px solid #bdbdbd;
   border-radius: 50%;
@@ -29,11 +28,11 @@ export default ({ commander, name }) => {
   if (commander && !colorIdentity.length) colorIdentity.push('C');
 
   return (
-    <StyledNameWrapepr>
+    <StyledNameWrapper level={3}>
       {name}
       {colorIdentity.map((color) => (
         <StyledColorIcon src={colors[color]} alt={color} key={color} />
       ))}
-    </StyledNameWrapepr>
+    </StyledNameWrapper>
   );
 };
