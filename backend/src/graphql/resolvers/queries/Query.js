10541cf3079a7d3e2305476fbfd8340fd3a0138b
Witte Hauke <hauke.witte@viu.ch>
2021-05-01 16:52:03 +0200

    feat (edhrec): add themes to graphql resolver

--- backend/src/graphql/resolvers/queries/Query.js
+++ backend/src/graphql/resolvers/queries/Query.js
@@ -57,8 +57,8 @@ const resolver = {
     return { id };
   },
 
-  edhrecCards(_, { names }) {
-    return getEdhrecCards(names);
+  edhrecCards(_, { names, themeSuffix }) {
+    return getEdhrecCards(names, themeSuffix);
   },
 
   async ownedCardNames(_, __, { db, user: { id: userId } }) {
--- backend/src/graphql/resolvers/queries/getEdhrecCards.js
+++ backend/src/graphql/resolvers/queries/getEdhrecCards.js
@@ -1,5 +1,18 @@
 import fetch from 'node-fetch';
 
+const getUrl = (names, themeSuffix) => {
+  const sanitizedNames = names
+    .join(' ')
+    .toLowerCase()
+    .replace(/\s\/\/.*$/, '')
+    .replace(/[^a-zA-Z ]/g, '')
+    .replace(/\s/g, '-');
+
+  const themeAddon = themeSuffix || '';
+
+  return `https://edhrec-json.s3.amazonaws.com/en/commanders/${sanitizedNames}${themeAddon}.json`;
+};
+
 const formatCards = cards => {
   return cards
     .map(({ cards, synergy, image_uris }) => {
@@ -19,30 +32,21 @@ const formatCards = cards => {
     .sort((a, b) => (b.synergy || 0) - (a.synergy || 0));
 };
 
-const fetchCards = async names => {
-  const sanitizedNames = names
-    .join(' ')
-    .toLowerCase()
-    .replace(/\s\/\/.*$/, '')
-    .replace(/[^a-zA-Z ]/g, '')
-    .replace(/\s/g, '-');
-  const url = `https://edhrec-json.s3.amazonaws.com/en/commanders/${sanitizedNames}.json`;
+const fetchCards = async (url, names) => {
   const result = await fetch(url);
   const json = await result.json();
 
   // partner commanders have to bee in a specific order
   // a redirect url is passed in those cases, but we can simply reverse the names as well
   if (json.redirect) {
-    return fetchCards(names.reverse());
+    const newUrl = getUrl(names.reverse());
+    return fetchCards(newUrl);
   }
+
   return json;
 };
 
-const getEdhrecCards = async names => {
-  if (!names.length) return [];
-
-  const json = await fetchCards(names);
-
+const getCardList = json => {
   const { cardlists } = json.container.json_dict;
 
   return cardlists?.map(({ header, cardviews, tag }) => ({
@@ -50,6 +54,32 @@ const getEdhrecCards = async names => {
     title: header,
     cards: formatCards(cardviews),
   }));
+};
+
+const getThemes = json => {
+  const { themes } = json.panels.tribelinks;
+
+  return themes?.map(tribe => ({
+    title: tribe.value,
+    urlSuffix: tribe['href-suffix'],
+    count: tribe.count,
+  }));
+};
+
+const getEdhrecCards = async (names, themeSuffix) => {
+  if (!names.length) return null;
+
+  const url = getUrl(names, themeSuffix);
+
+  const json = await fetchCards(url, names);
+
+  const cardLists = getCardList(json);
+  const themes = getThemes(json);
+
+  return {
+    cardLists,
+    themes,
+  };
 };
 
 export default getEdhrecCards;
