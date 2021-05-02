4acfdb464cd11fbf6755a17fb44a6c600b2fcf2c
Witte Hauke <hauke.witte@viu.ch>
2021-05-02 19:09:52 +0200

    feat (deck breakdown): split cmc chart by type

--- frontend/src/components/Desktop/Deck/DeckBreakdown/DeckbreakdownBody/DeckCmc.tsx
+++ frontend/src/components/Desktop/Deck/DeckBreakdown/DeckbreakdownBody/DeckCmc.tsx
@@ -1,6 +1,7 @@
-import { colorPalette } from 'constants/colors';
+import { CARD_TYPE_DECK_ORDER } from 'components/Provider/CardProvider/staticTypes';
+import { typeColors } from 'constants/colors';
 import React from 'react';
-import { BarChart, Bar, XAxis, Tooltip } from 'recharts';
+import { BarChart, Bar, XAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
 import { UnifiedDeck } from 'types/unifiedTypes';
 import { DeckStat } from './DeckStat';
@@ -8,21 +9,38 @@ import { DeckStat } from './DeckStat';
 interface Props {
   deck: UnifiedDeck;
 }
+
+const getPrimaryType = (primaryTypes: string[]) => {
+  return primaryTypes.sort((a, b) => {
+    if (CARD_TYPE_DECK_ORDER.indexOf(a) === -1) return 1;
+    if (CARD_TYPE_DECK_ORDER.indexOf(b) === -1) return -1;
+
+    return CARD_TYPE_DECK_ORDER.indexOf(a) - CARD_TYPE_DECK_ORDER.indexOf(b);
+  })[0];
+};
+
 export const DeckCmc = ({ deck }: Props) => {
   const manaValues = deck.cards
     .filter((card) => !card.primaryTypes.includes('Land'))
     .reduce((acc, card) => {
-      const { cmc } = card;
-      const newCount = (acc[cmc] ?? 0) + 1;
+      const { cmc, primaryTypes, amount } = card;
+
+      const primaryType = getPrimaryType(primaryTypes);
+
+      const cmcByType = acc[cmc] ?? {};
+      const currentCount = cmcByType[primaryType] ?? 0;
 
       return {
         ...acc,
-        [cmc]: newCount,
+        [cmc]: {
+          ...cmcByType,
+          [primaryType]: currentCount + amount,
+        },
       };
     }, {});
 
-  const data: { cmc: number; amount: number }[] = Object.entries(manaValues)
-    .map(([cmc, amount]) => ({ cmc: Number(cmc), amount }))
+  const data: { cmc: number; [key: string]: number }[] = Object.keys(manaValues)
+    .map((key) => ({ cmc: Number(key), ...manaValues[key] }))
     .sort((a, b) => a.cmc - b.cmc)
     .reduce((acc, val) => {
       if (!acc.length) return [val];
@@ -42,27 +60,31 @@ export const DeckCmc = ({ deck }: Props) => {
 
   return (
     <DeckStat title="Mana Curve:" hidden={!data.length}>
-      <BarChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 20,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <XAxis dataKey="cmc" />
-        <Tooltip />
-        <Bar
-          dataKey="amount"
-          stackId="a"
-          maxBarSize={50}
-          name="Number of Cards"
-          fill={colorPalette[15]}
-        />
-      </BarChart>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart
+          data={data}
+          margin={{
+            top: 20,
+            right: 20,
+            left: 20,
+            bottom: 20,
+          }}
+        >
+          <XAxis dataKey="cmc" />
+          <Tooltip />
+          {CARD_TYPE_DECK_ORDER.map((type) => (
+            <Bar
+              key={type}
+              dataKey={type}
+              stackId="a"
+              maxBarSize={50}
+              name={type}
+              fill={typeColors[type]}
+              isAnimationActive={false}
+            />
+          ))}
+        </BarChart>
+      </ResponsiveContainer>
     </DeckStat>
   );
 };
