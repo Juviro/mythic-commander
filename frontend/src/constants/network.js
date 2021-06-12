72269cf29b04c80a621d4a249fe2e30d906fbe5d
Witte Hauke <hauke.witte@viu.ch>
2021-06-12 19:39:20 +0200

    chore: backend address for home server

--- frontend/src/constants/network.js
+++ frontend/src/constants/network.js
@@ -1,6 +1,6 @@
 export const IS_DEV = process.env.NODE_ENV === 'development';
 
 export const SERVER_URL = IS_DEV
-  ? // ? 'http://192.168.178.179:4000/mtg-api/graphql'
-    'http://localhost:4000/mtg-api/graphql'
-  : '/mtg-api/graphql';
+  ? 'http://juviro.ddns.net:4000/mtg-api/graphql'
+  : // 'http://localhost:4000/mtg-api/graphql'
+    '/mtg-api/graphql';
