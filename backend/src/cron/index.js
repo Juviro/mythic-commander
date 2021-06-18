488ed974cbe011b5149d653c1d39a8150dd88b64
Witte Hauke <hauke.witte@viu.ch>
2021-06-18 14:00:45 +0200

    chore: change cronjob for testing

--- backend/src/cron/index.js
+++ backend/src/cron/index.js
@@ -6,7 +6,7 @@ import collectionSnapshot from './collectionSnapshot';
 
 const startCronjobs = () => {
   new CronJob('0 0 5 * * *', deleteSessions, null, true, 'Europe/Berlin');
-  new CronJob('0 0 14 * * *', updateCards, null, true, 'Europe/Berlin');
+  new CronJob('0 2 14 * * *', updateCards, null, true, 'Europe/Berlin');
   new CronJob('0 0 7 * * *', collectionSnapshot, null, true, 'Europe/Berlin');
 };
 startCronjobs();
