bc5992e611c656a563a38010775eda8af3b11db2
Witte Hauke <hauke.witte@viu.ch>
2021-12-05 19:37:08 +0100

    chore(Deck): always use type view as default

--- frontend/src/components/Desktop/Deck/Deck.tsx
+++ frontend/src/components/Desktop/Deck/Deck.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { useQuery, useMutation } from 'react-apollo';
 
@@ -11,7 +11,6 @@ import { getColorIdentity } from 'utils/commander';
 import Flex from 'components/Elements/Shared/Flex';
 import NotFound from 'components/Elements/Shared/NotFound';
 import ShortcutFocus from 'components/Elements/Shared/ShortcutFocus';
-import useLocalStorage from 'components/Hooks/useLocalStorage';
 import Cards from './Cards';
 import message from '../../../utils/message';
 import unifyCardFormat from '../../../utils/unifyCardFormat';
@@ -33,7 +32,7 @@ export default () => {
   });
   const [mutate] = useMutation<any, MutationAddCardsToDeckArgs>(addCardsToDeckDesktop);
 
-  const [view, setView] = useLocalStorage('type');
+  const [view, setView] = useState<View>('type');
 
   const deck = data?.deck;
   const cards = unifyCardFormat(deck?.cards);
