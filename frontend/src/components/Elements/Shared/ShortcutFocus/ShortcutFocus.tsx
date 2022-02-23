a575dd5e0e3d8c8d7bc9a8e963f8ea535a18a033
Hauke Witte <hauke.witte@viu.ch>
2022-02-23 18:21:07 +0100

    chore(ShortcutFocus): fix tsc

--- frontend/src/components/Elements/Shared/ShortcutFocus/ShortcutFocus.tsx
+++ frontend/src/components/Elements/Shared/ShortcutFocus/ShortcutFocus.tsx
@@ -1,27 +1,27 @@
-// import React from 'react';
-// import useShortcutFocus from '../../../Hooks/useShortcutFocus';
+/* eslint-disable */
+import React from 'react';
+import useShortcutFocus from '../../../Hooks/useShortcutFocus';
 
-// interface Props {
-//   children: React.ReactNode;
-//   focusId: string;
-//   visible?: boolean;
-//   style?: React.CSSProperties;
-// }
+interface Props {
+  children: React.ReactNode;
+  focusId: string;
+  visible?: boolean;
+  style?: React.CSSProperties;
+}
 
-// export default ({ children, focusId, visible, style }: Props) => {
-//   const onClick = useShortcutFocus(focusId, visible);
+export default ({ children, focusId, visible, style }: Props) => {
+  return <>children</>;
+  const onClick = useShortcutFocus(focusId, visible);
 
-//   return (
-//     <div
-//       onClick={(e) => {
-//         e.stopPropagation();
-//         onClick();
-//       }}
-//       style={style}
-//     >
-//       {children}
-//     </div>
-//   );
-// };
-
-export default ({ children }) => children;
+  return (
+    <div
+      onClick={(e) => {
+        e.stopPropagation();
+        onClick();
+      }}
+      style={style}
+    >
+      {children}
+    </div>
+  );
+};
