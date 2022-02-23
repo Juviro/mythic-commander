78ff6ca0d6103754109940103f8ce9fb1399c87e
Hauke Witte <hauke.witte@viu.ch>
2022-02-23 21:36:56 +0100

    feat(ImgService): use image service for normal sized cards in FE

--- frontend/src/utils/cardImage.ts
+++ frontend/src/utils/cardImage.ts
@@ -1,7 +1,13 @@
+import { IS_DEV } from '../constants/network';
+
 const NO_IMG_URL = 'https://c2.scryfall.com/file/scryfall-errors/soon.jpg';
 
 export const getImageUrl = (id: string, key: string, size = 'small', face = 'front') => {
   if (!id || !key) return NO_IMG_URL;
+
+  if (!IS_DEV && size === 'normal') {
+    return `/img/${id}_${face}.avif`;
+  }
 
   return `https://c1.scryfall.com/file/scryfall-cards/${size}/${face}/${key}/${id}.jpg`;
 };
