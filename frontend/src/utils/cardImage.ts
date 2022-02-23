284f47c98c1f6bf96275c7c72bee6ba6e98a97e9
Hauke Witte <hauke.witte@viu.ch>
2022-02-23 19:01:38 +0100

    chore: update scryfall image url

--- frontend/src/utils/cardImage.ts
+++ frontend/src/utils/cardImage.ts
@@ -3,5 +3,5 @@ const NO_IMG_URL = 'https://c2.scryfall.com/file/scryfall-errors/soon.jpg';
 export const getImageUrl = (id: string, key: string, size = 'small', face = 'front') => {
   if (!id || !key) return NO_IMG_URL;
 
-  return `https://img.scryfall.com/cards/${size}/${face}/${key}/${id}.jpg`;
+  return `https://c1.scryfall.com/file/scryfall-cards/${size}/${face}/${key}/${id}.jpg`;
 };
