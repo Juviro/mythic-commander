33de9f0ab31f6753a24f5575dd891e74e95b433e
Hauke Witte <hauke.witte@viu.ch>
2022-02-24 17:32:14 +0100

    feat(ImgService): always use own service

--- frontend/src/utils/cardImage.ts
+++ frontend/src/utils/cardImage.ts
@@ -5,10 +5,6 @@ const NO_IMG_URL = 'https://c2.scryfall.com/file/scryfall-errors/soon.jpg';
 export const getImageUrl = (id: string, key: string, size = 'small', face = 'front') => {
   if (!id || !key) return NO_IMG_URL;
 
-  if (size === 'normal') {
-    const baseUrl = IS_DEV ? 'https://mythic-commander.com/' : '/';
-    return `${baseUrl}img/${id}_${face}.avif`;
-  }
-
-  return `https://c1.scryfall.com/file/scryfall-cards/${size}/${face}/${key}/${id}.jpg`;
+  const baseUrl = IS_DEV ? 'https://mythic-commander.com/' : '/';
+  return `${baseUrl}img/${id}_${size}_${face}.avif`;
 };
