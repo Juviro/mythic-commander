cfa36a7bf1d76ccdc99ee5edce34b29cdd680cf7
Hauke Witte <hauke.witte@viu.ch>
2022-03-09 21:24:26 +0100

    fix: use scryfall for images again

--- frontend/src/utils/cardImage.ts
+++ frontend/src/utils/cardImage.ts
@@ -1,10 +1,12 @@
-import { IS_DEV } from '../constants/network';
+// import { IS_DEV } from '../constants/network';
 
 const NO_IMG_URL = 'https://c2.scryfall.com/file/scryfall-errors/soon.jpg';
 
 export const getImageUrl = (id: string, key: string, size = 'small', face = 'front') => {
   if (!id || !key) return NO_IMG_URL;
 
-  const baseUrl = IS_DEV ? 'https://mythic-commander.com/' : '/';
-  return `${baseUrl}img/${id}_${size}_${face}.avif`;
+  // const baseUrl = IS_DEV ? 'https://mythic-commander.com/' : '/';
+  // return `${baseUrl}img/${id}_${size}_${face}.avif`;
+
+  return `https://c1.scryfall.com/file/scryfall-cards/${size}/${face}/${key}/${id}.jpg`;
 };
