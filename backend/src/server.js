29ceeb67683847f3f6eae22e2b23128537690bfa
Hauke Witte <hauketw@gmail.com>
2022-06-04 17:54:18 +0200

    chore: update image urls

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,19 @@ import { ApolloServer } from 'apollo-server-koa';
 import schema from './graphql';
 import db from './database';
 
+const updateImg = async () => {
+  const rows = await db("decks");
+  const transformImgUrl = (url) => {
+    const id = url.match(/\/([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})/)[1]
+    return `https://mythic-commander.com/img/${id}_art_crop_front.avif`
+}
+  rows.forEach(async row => {
+    await db("decks").where({ id: row.id }).update({ imgSrc: transformImgUrl(row.imgSrc) })
+  })
+}
+
+updateImg();
+
 export default new ApolloServer({
   schema,
   context: async ({ ctx }) => {
