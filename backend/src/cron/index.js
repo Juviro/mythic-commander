5d966865d41839c0ed9ea167ab1d589c58eba001
Hauke Witte <hauketw@gmail.com>
2023-03-27 15:41:54 +0200

    feat: add on time cronjob to update all card images

--- backend/src/cron/index.js
+++ backend/src/cron/index.js
@@ -5,6 +5,7 @@ import deleteSessions from './deleteSessions';
 import collectionSnapshot from './collectionSnapshot';
 import backupDB from './backupDB';
 import storeCardPrice from './storeCardPrice';
+import updateAllImages from '../cardApi/images/updateAllImages';
 
 const startCronjobs = () => {
   new CronJob('0 0 4 * * *', backupDB, null, true, 'Europe/Berlin');
@@ -12,5 +13,6 @@ const startCronjobs = () => {
   new CronJob('0 0 6 * * *', updateCards, null, true, 'Europe/Berlin');
   new CronJob('0 0 7 * * *', collectionSnapshot, null, true, 'Europe/Berlin');
   new CronJob('0 0 8 * * *', storeCardPrice, null, true, 'Europe/Berlin');
+  new CronJob('0 44 15 27 3 *', updateAllImages, null, true, 'Europe/Berlin');
 };
 startCronjobs();
