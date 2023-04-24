e156e3e9a9462e3e302744d3af680284ad97849b
Hauke Witte <hauketw@gmail.com>
2023-04-24 21:35:12 +0200

    fix(search): fix searching for owned cards

--- backend/src/graphql/resolvers/queries/cardSearch.js
+++ backend/src/graphql/resolvers/queries/cardSearch.js
@@ -26,11 +26,12 @@ const addColorClause = (q, colors) => {
   }
 };
 
-const addOwnedClause = (q, userId, isOwned) => {
+const addOwnedClause = (q, userId, isOwned, tableName) => {
   const operator = isOwned === 'true' ? 'IN' : 'NOT IN';
+
   q.whereRaw(
-    `oracle_id ${operator} (SELECT DISTINCT oracle_id FROM "collectionWithOracle" WHERE "userId" = ?)`,
-    userId
+    `??.oracle_id ${operator} (SELECT DISTINCT ??.oracle_id FROM "collectionWithOracle" WHERE "userId" = ?)`,
+    [tableName, tableName, userId]
   );
 };
 
@@ -125,7 +126,8 @@ export default async (
     if (isLegendary === 'false')
       q.whereNot('type_line', 'ILIKE', `%Legendary%`);
     if (colors.length) addColorClause(q, colors);
-    if (isOwned && user && user.id) addOwnedClause(q, user.id, isOwned);
+    if (isOwned && user && user.id)
+      addOwnedClause(q, user.id, isOwned, tableName);
     if (cmc) addRangeClause(q, cmc, 'cmc');
     if (power) addRangeClause(q, power, 'power');
     if (toughness) addRangeClause(q, toughness, 'toughness');
