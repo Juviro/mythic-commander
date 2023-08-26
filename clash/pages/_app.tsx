3c482a8fe0eb23b2060b33422b8aa4ab9432f09e
Hauke Witte <hauketw@gmail.com>
2023-08-26 23:50:58 +0200

    chore(clash): add page title to all clash pages

--- clash/pages/_app.tsx
+++ clash/pages/_app.tsx
@@ -1,17 +1,26 @@
-import '../styles/globals.css';
-import '../styles/variables.css';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { ConfigProvider } from 'antd';
 
 import { SocketContextProvider } from 'components/SocketContext/SocketContextProvider';
 import antdTheme from '../styles/antdTheme';
 
+import '../styles/globals.css';
+import '../styles/variables.css';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SocketContextProvider>
-      <ConfigProvider theme={antdTheme}>
-        <Component {...pageProps} />
-      </ConfigProvider>
-    </SocketContextProvider>
+    <>
+      <Head>
+        <title>MYC Clash</title>
+        <meta name="description" content="Play EDH Online" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SocketContextProvider>
+        <ConfigProvider theme={antdTheme}>
+          <Component {...pageProps} />
+        </ConfigProvider>
+      </SocketContextProvider>
+    </>
   );
 }
--- clash/pages/index.tsx
+++ clash/pages/index.tsx
@@ -1,16 +1,9 @@
-import Head from 'next/head';
-
 import GameBrowser from '../components/GameBrowser/GameBrowser';
 import { GameBrowserContextProvider } from '../components/GameBrowser/GameBrowserContext';
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>MYC Clash</title>
-        <meta name="description" content="Play EDH Online" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <GameBrowserContextProvider>
         <GameBrowser />
       </GameBrowserContextProvider>
