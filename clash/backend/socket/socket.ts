cf4f188c442d8501d4edbe2b20381c7634109b60
Hauke Witte <hauketw@gmail.com>
2024-02-04 01:43:10 +0100

    chore upgrade socket.io

--- clash/backend/socket/socket.ts
+++ clash/backend/socket/socket.ts
@@ -3,6 +3,8 @@ import lobbySocketActions from 'backend/lobby/lobbySocketActions';
 import gameSocketActions from 'backend/game/gameSocketActions';
 
 const lobbysocket = (_: any, res: any) => {
+  // eslint-disable-next-line no-console
+  console.log('res.socket?.server.io', res.socket?.server.io);
   if (res.socket?.server.io) {
     res.end();
     return;
@@ -10,6 +12,8 @@ const lobbysocket = (_: any, res: any) => {
 
   const io = new Server(res.socket?.server);
   res.socket.server.io = io;
+  // eslint-disable-next-line no-console
+  console.log('io', io);
 
   lobbySocketActions(io);
   gameSocketActions(io);
