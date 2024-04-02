e2d1bddaf5ebebb43d8c55421278eb38a0265980
Hauke Witte <hauketw@gmail.com>
2024-04-02 10:43:19 +0200

    feat: allow minimizing search library popover

--- clash/components/GameComponents/ZoneCardsPopover/PopoverContent.tsx
+++ clash/components/GameComponents/ZoneCardsPopover/PopoverContent.tsx
@@ -1,6 +1,8 @@
 import React, { CSSProperties, useContext, useState } from 'react';
-
+import classNames from 'classnames';
+import { DownOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Input, Space } from 'antd';
+
 import GameStateContext from 'components/Game/GameStateContext';
 import { normalizeName } from 'utils/normalizeName';
 import useGameActions from 'components/Game/useGameActions';
@@ -34,6 +36,7 @@ const PopoverContent = ({ color, zone }: Props) => {
   const [randomizeBottomCards, setRandomizeBottomCards] = useState(true);
   const [shuffleLibrary, setShuffleLibrary] = useState(true);
   const [search, setSearch] = useState('');
+  const [isMinimized, setIsMinimized] = useState(false);
   const {
     cardsToTop,
     cardsToBottom,
@@ -75,74 +78,90 @@ const PopoverContent = ({ color, zone }: Props) => {
     });
 
   return (
-    <div className={styles.content} style={style as CSSProperties}>
-      <PopoverCardList
-        zone={zone}
-        cards={cardsToBottom}
-        onDrop={onDropBottom}
-        title="Bottom of Library"
-        empty="Drag Cards here to put them on the Bottom of the Library"
-        bottom={
-          <Checkbox
-            checked={randomizeBottomCards}
-            onChange={(e) => setRandomizeBottomCards(e.target.checked)}
-          >
-            Random order
-          </Checkbox>
-        }
-      />
-      {isSearch && (
+    <>
+      {isMinimized && <span className={styles.minimized_title}>Search Library</span>}
+      <div
+        className={classNames(styles.minimize_button, {
+          [styles.minimize_button__minimized]: isMinimized,
+        })}
+        onClick={() => setIsMinimized(!isMinimized)}
+      >
+        <DownOutlined />
+      </div>
+      <div
+        className={classNames(styles.content, {
+          [styles.content__minimized]: isMinimized,
+        })}
+        style={style as CSSProperties}
+      >
         <PopoverCardList
           zone={zone}
-          onDrop={onDropLibrary}
-          cards={filteredCards}
-          empty="No cards found"
+          cards={cardsToBottom}
+          onDrop={onDropBottom}
+          title="Bottom of Library"
+          empty="Drag Cards here to put them on the Bottom of the Library"
+          bottom={
+            <Checkbox
+              checked={randomizeBottomCards}
+              onChange={(e) => setRandomizeBottomCards(e.target.checked)}
+            >
+              Random order
+            </Checkbox>
+          }
+        />
+        {isSearch && (
+          <PopoverCardList
+            zone={zone}
+            onDrop={onDropLibrary}
+            cards={filteredCards}
+            empty="No cards found"
+            titleRight={<OrderLabel />}
+            title={
+              <Space size={16}>
+                <span>Library</span>
+                <Input
+                  placeholder="Search"
+                  autoFocus
+                  value={search}
+                  allowClear
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+                <Checkbox
+                  checked={landsOnly}
+                  onChange={(e) => setLandsOnly(e.target.checked)}
+                  style={{ fontWeight: 'normal' }}
+                >
+                  Only Lands with Basic Land Types
+                </Checkbox>
+              </Space>
+            }
+          />
+        )}
+        <PopoverCardList
+          zone={zone}
+          cards={cardsToTop}
+          onDrop={onDropTop}
+          title="Top of Library"
+          empty="Drag Cards here to put them on Top of the Library"
           titleRight={<OrderLabel />}
-          title={
+          bottom={
             <Space size={16}>
-              <span>Library</span>
-              <Input
-                placeholder="Search"
-                autoFocus
-                value={search}
-                allowClear
-                onChange={(e) => setSearch(e.target.value)}
-              />
-              <Checkbox
-                checked={landsOnly}
-                onChange={(e) => setLandsOnly(e.target.checked)}
-                style={{ fontWeight: 'normal' }}
-              >
-                Only Lands with Basic Land Types
-              </Checkbox>
+              {isSearch && (
+                <Checkbox
+                  checked={shuffleLibrary}
+                  onChange={(e) => setShuffleLibrary(e.target.checked)}
+                >
+                  Shuffle Library
+                </Checkbox>
+              )}
+              <Button type="primary" onClick={onSubmit}>
+                Done
+              </Button>
             </Space>
           }
         />
-      )}
-      <PopoverCardList
-        zone={zone}
-        cards={cardsToTop}
-        onDrop={onDropTop}
-        title="Top of Library"
-        empty="Drag Cards here to put them on Top of the Library"
-        titleRight={<OrderLabel />}
-        bottom={
-          <Space size={16}>
-            {isSearch && (
-              <Checkbox
-                checked={shuffleLibrary}
-                onChange={(e) => setShuffleLibrary(e.target.checked)}
-              >
-                Shuffle Library
-              </Checkbox>
-            )}
-            <Button type="primary" onClick={onSubmit}>
-              Done
-            </Button>
-          </Space>
-        }
-      />
-    </div>
+      </div>
+    </>
   );
 };
 
