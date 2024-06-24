2476dd7382d8323e9bf79c8c1ee3b2f6d57c71a7
Hauke Witte <hauketw@gmail.com>
2024-06-24 16:53:43 +0200

    fix(Sidebar): take bottom action bar into calculation

--- frontend/src/components/Desktop/Deck/Sidebar/Sidebar.tsx
+++ frontend/src/components/Desktop/Deck/Sidebar/Sidebar.tsx
@@ -44,7 +44,8 @@ export default ({ currentTabId, setCurrentTabId, onAddCards, deck }: Props) => {
           padding: 0,
         },
         wrapper: {
-          height: 'calc(100% - 48px)',
+          // 48px navbar height, 64px action bar height
+          height: 'calc(100% - 48px - 64px)',
           marginTop: 48,
         },
       }}
