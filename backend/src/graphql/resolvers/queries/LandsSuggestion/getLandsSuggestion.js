b5e762924c1f8b46ec20549c02f6c18f9d4c2392
Hauke Witte <hauketw@gmail.com>
2024-06-24 16:46:22 +0200

    chore: disable eslint in wip landSuggestion

--- backend/src/graphql/resolvers/queries/LandsSuggestion/getLandsSuggestion.js
+++ backend/src/graphql/resolvers/queries/LandsSuggestion/getLandsSuggestion.js
@@ -4,7 +4,9 @@ import { getCards, getColorIdentity } from './landSuggestionHelpers';
 const getLandsSuggestion = async (deckId, options, userId) => {
   await canEditDeck(userId, deckId);
 
+  // eslint-disable-next-line no-unused-vars
   const colorIdentity = await getColorIdentity(deckId);
+  // eslint-disable-next-line no-unused-vars
   const cards = await getCards(deckId);
 
   return {
