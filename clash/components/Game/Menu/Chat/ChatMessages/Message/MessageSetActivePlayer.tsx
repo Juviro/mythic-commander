5f05f11760df3816b104be26f9d7d86920078346
Hauke Witte <hauketw@gmail.com>
2024-09-01 22:40:14 +0200

    feat(clash): make chat message for active player change more visible

--- clash/components/Game/Menu/Chat/ChatMessages/Message/MessageSetActivePlayer.tsx
+++ clash/components/Game/Menu/Chat/ChatMessages/Message/MessageSetActivePlayer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { LogPayloadSetActivePlayer } from 'backend/constants/logMessages';
+import { Divider } from 'antd';
 import ColoredPlayerName from '../../../../../GameComponents/ColoredPlayerName/ColoredPlayerName';
 
 import styles from '../../Chat.module.css';
@@ -12,9 +13,11 @@ interface Props {
 const MessageSetActivePlayer = ({ payload: { activePlayerId } }: Props) => {
   return (
     <div className={styles.message}>
-      {`It's `}
-      <ColoredPlayerName id={activePlayerId} addGenetiveSuffix />
-      {` turn `}
+      <Divider orientation="left" className={styles.divider}>
+        {`It's `}
+        <ColoredPlayerName id={activePlayerId} addGenetiveSuffix />
+        {` turn `}
+      </Divider>
     </div>
   );
 };
