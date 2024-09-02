34206d11c72609fdc287f6faff3b5fda0adfd6f5
Hauke Witte <hauketw@gmail.com>
2024-09-02 17:21:27 +0200

    feat(clash): add setting to auto reduce life on commander damage

--- clash/components/Game/Menu/GameInfo/GuideModal/Settings.tsx
+++ clash/components/Game/Menu/GameInfo/GuideModal/Settings.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import useLocalStorage from 'hooks/useLocalStorage';
 import Setting from './Setting';
@@ -10,11 +10,13 @@ export const SETTINGS_STORAGE_KEY = 'settings';
 interface Settings {
   autoUntap: boolean;
   useGrid: boolean;
+  commanderDamageChangesLife: boolean;
 }
 
 const DEFAULT_SETTINGS: Settings = {
   autoUntap: true,
   useGrid: true,
+  commanderDamageChangesLife: true,
 };
 
 const Settings = () => {
@@ -22,6 +24,18 @@ const Settings = () => {
     SETTINGS_STORAGE_KEY,
     DEFAULT_SETTINGS
   );
+
+  // make sure new settings are added to local storage
+  useEffect(() => {
+    const numberOfSettings = Object.keys(settings).filter(
+      // make sure we don't count old settings that are no longer used
+      (key) => DEFAULT_SETTINGS[key as keyof Settings] !== undefined
+    ).length;
+
+    if (numberOfSettings === Object.keys(DEFAULT_SETTINGS).length) return;
+
+    setSettings({ ...DEFAULT_SETTINGS, ...settings });
+  }, []);
 
   const onChangeSetting = (key: keyof Settings) => (value: boolean) => {
     setSettings({ ...settings, [key]: value });
@@ -31,7 +45,7 @@ const Settings = () => {
     <div>
       <h2 className={styles.guide_title}>Game</h2>
       <Setting
-        label="Untap lands when it's your turn"
+        label="Untap all lands at the beginngin of your turn"
         checked={settings.autoUntap}
         onChange={onChangeSetting('autoUntap')}
       />
@@ -39,6 +53,11 @@ const Settings = () => {
         label="Snap cards to grid"
         checked={settings.useGrid}
         onChange={onChangeSetting('useGrid')}
+      />
+      <Setting
+        label="Changing commander damage also changes life total"
+        checked={settings.commanderDamageChangesLife}
+        onChange={onChangeSetting('commanderDamageChangesLife')}
       />
     </div>
   );
